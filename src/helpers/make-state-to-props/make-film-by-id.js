88a55ac02e9df09fddfd1ae50e15446dd6d126c9
VorgSpring <drmrSP@yandex.ru>
2020-04-03 01:12:21 +0300

    delete makeFilmById

--- src/helpers/make-state-to-props/make-film-by-id.js
+++
@@ -1,9 +0,0 @@
-import {makeFilmByIdSelector} from '../../selectors/films/films';
-
-export const makeFilmById = () => {
-  const getFilmByIdSelector = makeFilmByIdSelector();
-  const mapStateToProps = (state, props) => ({
-    film: getFilmByIdSelector(state, props)
-  });
-  return mapStateToProps;
-};
