00dfa5f527aa292f669f0bf31a943c29f0cce797
VorgSpring <drmrSP@yandex.ru>
2020-04-03 02:11:19 +0300

    add More like this

--- src/components/movie-page/movie-page.test.js
+++ src/components/movie-page/movie-page.test.js
@@ -3,13 +3,22 @@ import {BrowserRouter} from 'react-router-dom';
 import {MoviePage} from './movie-page.jsx';
 import renderer from 'react-test-renderer';
 import {Provider} from 'react-redux';
+import thunk from "redux-thunk";
+import {createAPI} from '../../api';
+import MockAdapter from 'axios-mock-adapter';
 import {state} from '../../mocks/state';
+import {ApiPaths} from '../../constants/api';
 import configureStore from 'redux-mock-store';
 import {films} from '../../mocks/films';
 
 describe(`MoviePage`, () => {
   it(`should renders correctly`, () => {
-    const mockStore = configureStore([]);
+    const api = createAPI();
+    const apiMock = new MockAdapter(api);
+    apiMock
+      .onGet(ApiPaths.FILMS)
+      .reply(200, []);
+    const mockStore = configureStore([thunk.withExtraArgument(api)]);
     const store = mockStore(state);
 
     const tree = renderer.create(
